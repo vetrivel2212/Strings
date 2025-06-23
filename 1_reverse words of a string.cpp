c2bec2d17f730251e04020dd9fbc003c3aea94c5
vetrivel2212 <vetrivel221205@gmail.com>
2025-06-23 11:20:06 +0530

    Update 1_reverse words of a string.cpp

--- 1_reverse words of a string.cpp
+++ 1_reverse words of a string.cpp
@@ -25,6 +25,50 @@
 //             result += words[i];
 //             if (i != words.size() - 1) result += " ";
 //         }
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    string cleanspaces(string s) {
+        int i = 0, j = 0, n = s.size();
+
+        while (i < n && s[i] == ' ') i++;
+
+        while (i < n) {
+            while (i < n && s[i] != ' ')
+                s[j++] = s[i++];
+
+            if (i < n && s[i] == ' ') {
+                s[j++] = ' ';
+                while (i < n && s[i] == ' ') i++;
+            }
+        }
+
+        if (j > 0 && s[j - 1] == ' ') j--;
+
+        s.resize(j);
+        return s;
+    }
+
+    string reverseWords(string s) {
+        s = cleanspaces(s);                    // Step 1: clean spaces
+        reverse(s.begin(), s.end());           // Step 2: reverse entire string
+
+        int n = s.size();
+        int start = 0;
+
+        // Step 3: reverse each word
+        for (int end = 0; end <= n; end++) {
+            if (end == n || s[end] == ' ') {
+                reverse(s.begin() + start, s.begin() + end);
+                start = end + 1;
+            }
+        }
+
+        return s;
+    }
+};
 
 //         return result;
 //     }
