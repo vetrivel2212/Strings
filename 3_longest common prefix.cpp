f4e973a52381f7c95b324c11205a11f018b8b847
vetrivel2212 <vetrivel221205@gmail.com>
2025-06-23 15:34:47 +0530

    Update 3_longest common prefix.cpp

--- 3_longest common prefix.cpp
+++ 3_longest common prefix.cpp
@@ -1,18 +1,33 @@
-class Solution {
-public:
-    string longestCommonPrefix(vector<string>& strs) {
-        if (strs.empty()) return "";
+// class Solution {
+// public:
+//     string longestCommonPrefix(vector<string>& strs) {
+//         if (strs.empty()) return "";
 
-        for (int i = 0; i < strs[0].size(); i++) {
-            char c = strs[0][i]; // pick character from first string
+//         for (int i = 0; i < strs[0].size(); i++) {
+//             char c = strs[0][i]; // pick character from first string
 
-            for (int j = 1; j < strs.size(); j++) {
-                // If index out of bounds or char mismatch
-                if (i >= strs[j].size() || strs[j][i] != c)
-                    return strs[0].substr(0, i);
-            }
-        }
+//             for (int j = 1; j < strs.size(); j++) {
+//                 // If index out of bounds or char mismatch
+//                 if (i >= strs[j].size() || strs[j][i] != c)
+//                     return strs[0].substr(0, i);
+string longestCommonPrefix(vector<string>& strs) {
+    if (strs.empty()) return "";
 
-        return strs[0]; // all characters matched
-    }
-};
+    sort(strs.begin(), strs.end());
+
+    string first = strs[0];
+    string last = strs[strs.size() - 1];
+
+    int i = 0;
+    while (i < first.size() && i < last.size() && first[i] == last[i])
+        i++;
+
+    return first.substr(0, i);
+}
+
+//             }
+//         }
+
+//         return strs[0]; // all characters matched
+//     }
+// };
