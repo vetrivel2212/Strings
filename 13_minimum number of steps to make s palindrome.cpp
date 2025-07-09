7d53db5fc21444f0fede78b349439a26751bc39f
vetrivel2212 <vetrivel221205@gmail.com>
2025-07-09 23:08:08 +0530

    Create 13_minimum number of steps to make s palindrome.cpp

---
+++ 13_minimum number of steps to make s palindrome.cpp
@@ -0,0 +1,20 @@
+int minInsertions(string s) {
+    int n = s.length();
+    string rev = s;
+    reverse(rev.begin(), rev.end());
+
+    // DP to find LCS of s and reverse(s) => gives LPS
+    vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=n; j++) {
+            if(s[i-1] == rev[j-1])
+                dp[i][j] = 1 + dp[i-1][j-1];
+            else
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+
+    int lps = dp[n][n];
+    return n - lps;  // Minimum insertions
+}
